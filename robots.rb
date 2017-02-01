0e4a70dba7cbc2bd4cdfd1e8c9513d1739531630
Laura Moore <rileyle@gmail.com>
2017-02-01 14:06:46 -0500

    Changed filed name per assignment instructions

---
+++ robots.rb
@@ -0,0 +1,67 @@
+
+
+# Explorer Mode
+class Robot
+
+  def initialize(name, height=10)
+    @name = name
+  end
+
+  def say_hi
+    "Hi!"
+  end
+
+  def say_name
+    "My name is #{@name}"
+  end
+
+end
+
+rob = Robot.new("Rob")
+
+
+
+
+class BendingUnit < Robot
+
+  def bend(object_to_bend)
+    puts "Bend #{object_to_bend}!"
+  end
+
+end
+
+bender = BendingUnit.new("flexo")
+
+
+
+
+class ActorUnit < Robot
+
+  def change_name(newname)
+    @name = newname
+    puts "Your new name is #{@name}!"
+  end
+
+end
+
+calc = ActorUnit.new("Calc")
+puts calc.change_name("Balculon")
+
+
+
+# Adventure Mode
+our_class = ["Kalea", "Nancy", "David", "Ron", "Laura", "Dave", "Demtra", "Kendrick", "Phil", "Ben", "Michael", "Miguel"]
+
+robot_class = our_class.collect { |student| Robot.new(student) }
+puts robot_class
+
+
+
+
+
+
+# Epic Mode
+
+
+
+# Randomly assign the class of the robot on creation.
